dc754ddbdb0f8442081ce854e03c819ffcdcd5a2
mdflynn <mdflynn34@outlook.com>
2021-03-09 15:16:04 -0700

    add constellation effect

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const partcilesArray = [];
+let hue = 0;
 
 //  handles canvas resizing when a user changes page dimensions
 window.addEventListener("resize", () => {
@@ -22,56 +23,78 @@ canvas.addEventListener("click", (event) => {
   // sets x, y coordinates globally
   mouse.x = event.x;
   mouse.y = event.y;
+  for (let i = 0; i < 5; i++) {
+    partcilesArray.push(new Particle());
+  }
 });
 
 canvas.addEventListener("mousemove", (event) => {
   mouse.x = event.x;
   mouse.y = event.y;
+  for (let i = 0; i < 10; i++) {
+    partcilesArray.push(new Particle());
+  }
 });
 
 class Particle {
   constructor() {
-    // this.x = mouse.x;
-    // this.y = mouse.y;
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 5 + 1;
-    this.speedX = Math.random() * 3 - 1.5
-    this.speedY = Math.random() * 3 - 1.5
+    this.x = mouse.x;
+    this.y = mouse.y;
+    this.size = Math.random() * 15 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+    this.color = "hsl(" + hue + ", 100%, 50%)";
   }
 
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
+    if (this.size > 0.2) {
+      this.size -= 0.1;
+    }
   }
 
   draw() {
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = this.color;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
   }
 }
 
-const init = () => {
-  for (let i = 0; i < 100; i++) {
-    partcilesArray.push(new Particle())
-  }
-}
-
-init()
-
 const handleParticles = () => {
   for (let i = 0; i < partcilesArray.length; i++) {
     partcilesArray[i].update();
-    partcilesArray[i].draw()
+    partcilesArray[i].draw();
+
+    for (let j = i; j < partcilesArray.length; j++) {
+      const dx = partcilesArray[i].x - partcilesArray[j].x;
+      const dy = partcilesArray[i].y - partcilesArray[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+      if (distance < 100) {
+        ctx.beginPath(); // draw a line
+        ctx.strokeStyle = partcilesArray[i].color;
+        ctx.lineWidth = 0.2;
+        ctx.moveTo(partcilesArray[i].x, partcilesArray[i].y);
+        ctx.lineTo(partcilesArray[j].x, partcilesArray[j].y);
+        ctx.stroke(); // actually draw line on canvas
+        ctx.closePath();
+      }
+    }
+    if (partcilesArray[i].size <= 0.3) {
+      partcilesArray.splice(i, 1);
+      i--;
+    }
   }
-}
+};
 
 const animate = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  handleParticles()
-  requestAnimationFrame(animate)
-}
+  // ctx.fillStyle = "rgba(0, 0, 0, 0.02)";
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
+  hue += 2;
+  requestAnimationFrame(animate);
+};
 
-animate()
+animate();
